d5ff93c3d1e48cd9b1779ba24474b2534a1535e2
PKinnaree <kinnariparmar818@gmail.com>
2022-03-28 22:27:43 -0400

    updated ExelUtils

--- src/main/java/com/pom/qa/util/ExcelUtils.java
+++ src/main/java/com/pom/qa/util/ExcelUtils.java
@@ -11,12 +11,6 @@ public class ExcelUtils extends TestBase {
 
 	}
 
-	/*public static void takeScreenshotAtEndOfTest() throws IOException {
-		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		String currentDir = System.getProperty("user.dir");
-		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
-	}*/
-
 	public static long PAGE_LOAD_TIMEOUT = 30;
 	public static long IMPLICIT_WAIT = 10;
 
